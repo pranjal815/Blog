ec148dbfbfd7951d2ae60fc3cd9a37b1000c6342
Pranjal Prakash Srivastava <pranjaltou@gmail.com>
2017-03-08 19:51:49 -0600

    Adding Capfile

---
+++ config/routes.rb
@@ -0,0 +1,25 @@
+Rails.application.routes.draw do
+
+  devise_for :authors
+  root to: 'blog/posts#index'
+
+  #Contact us Page routes
+  match '/contact', to: 'contacts#new', via: 'get'
+  resources "contacts", only: [:new, :create]
+
+  namespace :authors do
+    resources :posts do
+      put 'published' => 'posts#publish', on: :member
+      put 'unpublished' => 'posts#unpublish', on: :member
+    end
+  end
+
+  scope module: 'blog' do
+    get 'about' => 'pages#about', as: :about
+    #get 'contact' => 'pages#contact', as: :contact
+    get 'posts' => 'posts#index', as: :posts
+    get 'posts/:id' => 'posts#show', as: :post
+  end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
