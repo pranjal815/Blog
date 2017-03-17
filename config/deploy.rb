d60f0be98ba7d36cf46f9fca32a4e14de4fe8de3
Pranjal Prakash Srivastava <pranjaltou@gmail.com>
2017-03-17 07:04:27 -0600

    Adding changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.7.2"
 
 set :application, 'urlshortner'
-set :repo_url, 'git@github.com:rkmmanivannan/rails-ec2-deploy.git' # Edit this to match your repository
+set :repo_url, 'git@github.com:pranjal815/Blog.git' # Edit this to match your repository
 set :branch, :master
 set :deploy_to, '/home/deploy/urlshortner'
 set :pty, true
